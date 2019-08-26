772b42c94a168b4b8c06a803a78328c40c5f75a6
wkamar <engkamar@gmail.com>
2019-08-26 21:36:25 +0200

    Updates

---
+++ precache-manifest.24f61f9d50f880fde018482be2383cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b981f8713279f4040d1759afe290f9f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5d8d3e3b295fb46d3a",
+    "url": "/static/css/main.7ed700e9.chunk.css"
+  },
+  {
+    "revision": "7dd40ed6568b9fb871bf",
+    "url": "/static/js/2.d6a0ffd1.chunk.js"
+  },
+  {
+    "revision": "ed5d8d3e3b295fb46d3a",
+    "url": "/static/js/main.b000ebb2.chunk.js"
+  },
+  {
+    "revision": "267c13d1c589c05897bb",
+    "url": "/static/js/runtime~main.28c51675.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
